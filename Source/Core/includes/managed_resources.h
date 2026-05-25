15a1285af168b54af98f1c86020efe4e50d7c3d2
garamond13 <98652255+garamond13@users.noreply.github.com>
2026-05-25 02:21:04 +0200

    Luma: Add early/experimental LumaTexture.

--- Source/Core/includes/managed_resources.h
+++ Source/Core/includes/managed_resources.h
@@ -5,6 +5,157 @@
 #include <vector>
 #include "com_ptr.h"
 
+// TODO: copy/move.
+// TODO: Move this somewhere else.
+// TODO: Handle views per MIP?
+// TODO: Handle DSV?
+// TODO: Handle multiple views?
+// TODO: Handle texture arrays?
+// TODO: Give up on all this?
+class LumaTexture
+{
+public:
+
+    LumaTexture(ID3D11Device* device, const D3D11_TEXTURE1D_DESC* tex_desc, const D3D11_SUBRESOURCE_DATA* initial_data = nullptr, const D3D11_SHADER_RESOURCE_VIEW_DESC* srv_desc = nullptr, const D3D11_RENDER_TARGET_VIEW_DESC* rtv_desc = nullptr, const D3D11_UNORDERED_ACCESS_VIEW_DESC* uav_desc = nullptr)
+    {
+        Create(device, tex_desc, initial_data, srv_desc, rtv_desc, uav_desc);
+    }
+
+    LumaTexture(ID3D11Device* device, const D3D11_TEXTURE2D_DESC* tex_desc, const D3D11_SUBRESOURCE_DATA* initial_data = nullptr, const D3D11_SHADER_RESOURCE_VIEW_DESC* srv_desc = nullptr, const D3D11_RENDER_TARGET_VIEW_DESC* rtv_desc = nullptr, const D3D11_UNORDERED_ACCESS_VIEW_DESC* uav_desc = nullptr)
+    {
+        Create(device, tex_desc, initial_data, srv_desc, rtv_desc, uav_desc);
+    }
+
+    LumaTexture(ID3D11Device* device, const D3D11_TEXTURE3D_DESC* tex_desc, const D3D11_SUBRESOURCE_DATA* initial_data = nullptr, const D3D11_SHADER_RESOURCE_VIEW_DESC* srv_desc = nullptr, const D3D11_RENDER_TARGET_VIEW_DESC* rtv_desc = nullptr, const D3D11_UNORDERED_ACCESS_VIEW_DESC* uav_desc = nullptr)
+    {
+        Create(device, tex_desc, initial_data, srv_desc, rtv_desc, uav_desc);
+    }
+
+ public:
+
+     void Create(ID3D11Device* device, const D3D11_TEXTURE1D_DESC* tex_desc, const D3D11_SUBRESOURCE_DATA* initial_data = nullptr, const D3D11_SHADER_RESOURCE_VIEW_DESC* srv_desc = nullptr, const D3D11_RENDER_TARGET_VIEW_DESC* rtv_desc = nullptr, const D3D11_UNORDERED_ACCESS_VIEW_DESC* uav_desc = nullptr)
+    {
+        ensure(device->CreateTexture1D(tex_desc, initial_data, (ID3D11Texture1D**)&texture), >= 0);
+        CreateViews(device, tex_desc->BindFlags, srv_desc, rtv_desc, uav_desc);
+    }
+
+    void Create(ID3D11Device* device, const D3D11_TEXTURE2D_DESC* tex_desc, const D3D11_SUBRESOURCE_DATA* initial_data = nullptr, const D3D11_SHADER_RESOURCE_VIEW_DESC* srv_desc = nullptr, const D3D11_RENDER_TARGET_VIEW_DESC* rtv_desc = nullptr, const D3D11_UNORDERED_ACCESS_VIEW_DESC* uav_desc = nullptr)
+    {
+        ensure(device->CreateTexture2D(tex_desc, initial_data, (ID3D11Texture2D**)&texture), >= 0);
+        CreateViews(device, tex_desc->BindFlags, srv_desc, rtv_desc, uav_desc);
+    }
+
+    void Create(ID3D11Device* device, const D3D11_TEXTURE3D_DESC* tex_desc, const D3D11_SUBRESOURCE_DATA* initial_data = nullptr, const D3D11_SHADER_RESOURCE_VIEW_DESC* srv_desc = nullptr, const D3D11_RENDER_TARGET_VIEW_DESC* rtv_desc = nullptr, const D3D11_UNORDERED_ACCESS_VIEW_DESC* uav_desc = nullptr)
+    {
+        ensure(device->CreateTexture3D(tex_desc, initial_data, (ID3D11Texture3D**)&texture), >= 0);
+        CreateViews(device, tex_desc->BindFlags, srv_desc, rtv_desc, uav_desc);
+    }
+
+    ID3D11Resource* GetResource() const
+    {
+        return (ID3D11Resource*)texture;
+    }
+
+    ID3D11ShaderResourceView* GetSRV() const
+    {
+        return srv;
+    }
+
+    ID3D11RenderTargetView* GetRTV() const
+    {
+        return rtv;
+    }
+
+    ID3D11UnorderedAccessView* GetUAV() const
+    {
+        return uav;
+    }
+
+    void GetTextureDesc(D3D11_TEXTURE1D_DESC* desc) const
+    {
+        ((ID3D11Texture1D*)texture)->GetDesc(desc);
+    }
+
+    void GetTextureDesc(D3D11_TEXTURE2D_DESC* desc) const
+    {
+        ((ID3D11Texture2D*)texture)->GetDesc(desc);
+    }
+
+    void GetTextureDesc(D3D11_TEXTURE3D_DESC* desc) const
+    {
+        ((ID3D11Texture3D*)texture)->GetDesc(desc);
+    }
+
+    void Reset()
+    {
+        if (texture)
+        {
+            ((IUnknown*)texture)->Release();
+            texture = nullptr;
+        }
+        if (srv)
+        {
+            srv->Release();
+            srv = nullptr;
+        }
+        if (rtv)
+        {
+            rtv->Release();
+            rtv = nullptr;
+        }
+        if (uav)
+        {
+            uav->Release();
+            uav = nullptr;
+        }
+    }
+
+public:
+
+    ~LumaTexture()
+    {
+        if (texture)
+        {
+            ((IUnknown*)texture)->Release();
+        }
+        if (srv)
+        {
+            srv->Release();
+        }
+        if (rtv)
+        {
+            rtv->Release();
+        }
+        if (uav)
+        {
+            uav->Release();
+        }
+    }
+
+private:
+
+    void CreateViews(ID3D11Device* device, UINT bind_flags, const D3D11_SHADER_RESOURCE_VIEW_DESC* srv_desc = nullptr, const D3D11_RENDER_TARGET_VIEW_DESC* rtv_desc = nullptr, const D3D11_UNORDERED_ACCESS_VIEW_DESC* uav_desc = nullptr)
+    {
+        if (bind_flags & D3D11_BIND_SHADER_RESOURCE)
+        {
+            ensure(device->CreateShaderResourceView((ID3D11Resource*)texture, srv_desc, &srv), >= 0);
+        }
+        if (bind_flags & D3D11_BIND_RENDER_TARGET)
+        {
+            ensure(device->CreateRenderTargetView((ID3D11Resource*)texture, rtv_desc, &rtv), >= 0);
+        }
+        if (bind_flags & D3D11_BIND_UNORDERED_ACCESS)
+        {
+            ensure(device->CreateUnorderedAccessView((ID3D11Resource*)texture, uav_desc, &uav), >= 0);
+        }
+    }
+
+    void* texture = nullptr;
+    ID3D11ShaderResourceView* srv = nullptr;
+    ID3D11RenderTargetView* rtv = nullptr;
+    ID3D11UnorderedAccessView* uav = nullptr;
+};
+
 struct ManagedResources
 {
     // Key should be result of CompileTimeStringHash()!
