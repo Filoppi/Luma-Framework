d0337f7b7871056f7bfc5fafd6683fc0dd40d8c7
Henrique da Gama Saczkowski <henrique1443@hotmail.com>
2026-09-14 23:16:54 -0300

    feat(fc5): add DLSS and DLAA support

--- Source/Core/core.hpp
+++ Source/Core/core.hpp
@@ -8820,7 +8820,7 @@ namespace
       {
       default:
       break;
-#if !defined(GAME_PERSONA_5_ROYAL) && !defined(GAME_METAPHOR_REFANTAZIO)
+#if !defined(GAME_PERSONA_5_ROYAL) && !defined(GAME_METAPHOR_REFANTAZIO) && !defined(GAME_FAR_CRY_5)
       case reshade::api::descriptor_type::texture_unordered_access_view:
       case reshade::api::descriptor_type::texture_shader_resource_view:
       {
